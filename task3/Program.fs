05e030fd9e37e96efe8a4d2a11285083631978f6
LpSymons <71932780+LpSymons@users.noreply.github.com>
2022-05-05 23:07:43 +0100

    task 4 still needs work

---
+++ task3/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+//Comment----- 
+type Account = {AccountNumber:int; Balance: float;}
+
+
+let account1 = {AccountNumber=0102; Balance=5.00}
+let account2 = {AccountNumber=0103; Balance=10.00}
+let account3 = {AccountNumber=0104; Balance=51.00}
+let account4 = {AccountNumber=0105; Balance=100.00}
+let account5 = {AccountNumber=0106; Balance=150.00}
+let account6 = {AccountNumber=0107; Balance=75.00}
+
+let accounts = [account1; account2; account3; account4; account5; account6;]
+
+let sequence1 = seq {for n in accounts do if (n.Balance >= 00.00 && n.Balance < 50.00) then n} 
+
+let sequence2 = seq {for n in accounts do if (n.Balance > 50.00) then n} 
+    
+//fun
+let f (x : seq<Account>) =
+    for n in x do 
+        let header = if (n.Balance >= 00.00 && n.Balance < 50.00) then "Sequence 1 : " else "Sequence 2 : "
+        printfn "%s%d" header n.AccountNumber    
+
+f sequence1 
+f sequence2
---
+++ task4/Program.fs
@@ -0,0 +1,41 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.Threading
+
+let myLock = ref 1
+
+type Ticket = {seat:int; customer:string}
+
+let mutable ticket = [for n in 1..10 -> {Ticket.seat = n; Ticket.customer = ""}]
+
+let DisplayTickets (x : Ticket list) =
+    for n in x do 
+        printfn "%d -> %s" n.seat n.customer
+
+
+DisplayTickets ticket
+
+let BookSeat _ =
+    lock(myLock)(fun()->
+                        Console.WriteLine("Please enter seat number")
+                        let seat = Console.ReadLine() |> int
+                        Console.WriteLine("Please enter your name")
+                        let name = Console.ReadLine()
+                        let newticket = {seat=seat; customer=name}
+                        //list.map help!!!
+                        printfn "%s" "seat does not exist")
+
+
+ThreadPool.QueueUserWorkItem(new WaitCallback(BookSeat)) |> ignore
+
+ThreadPool.QueueUserWorkItem(new WaitCallback(BookSeat)) |> ignore
+
+Thread.Sleep(10000)
+
+DisplayTickets ticket
+
+(*
+for n in ticket do 
+    if n.seat = seat then ticket.[seat] = newticket |>ignore 
+    else printfn "%s" "seat does not exist" ) *)
