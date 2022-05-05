16ba599e795f6a47d2a91fbce97e63093a2d4a5b
LpSymons <71932780+LpSymons@users.noreply.github.com>
2022-05-05 18:35:43 +0100

    Added comments and removed unused code

--- task2/Program.fs
+++ task2/Program.fs
@@ -2,25 +2,14 @@
 
 open System
 
-//Record for information about an account
-//type Account = { AccountNumber: int; mutable Balance: float; Name: string;}
-
-//let account = 
-//    { AccountNumber = 0001
-//      Balance = 5.00
-//      Name = "Joe"}
-
-//let account2 = 
-//    { AccountNumber = 0002
-//      Balance = 100.00
-//      Name = "Andy"}
-
+//Record storage of types 
 type Accounts = {AccountNumber:int; Balance: float;}
 
+//Record storage thats stores users of type account
 type User = 
     | User of Accounts
 
-
+//Method that prints a users account number and balance and prints appropriate message
 let checkAccount account =
     match account with 
     | User {AccountNumber=n;Balance=b;} -> if b < 10.00 then
@@ -33,59 +22,22 @@ let checkAccount account =
                                                 printfn $"Account Number:{n} Balance: {b}"
                                                 Console.WriteLine("Balance is High")
 
- 
-
-
-User {AccountNumber=0102; Balance=5.00 }
+ //Users added to record and then passed to checkAccount method
+User {AccountNumber=0102; Balance=5.00}
 |> checkAccount
-
+ //Users added to record and then passed to checkAccount method
 User {AccountNumber=0103; Balance=10.00}
 |> checkAccount
-
+ //Users added to record and then passed to checkAccount method
 User {AccountNumber=0104; Balance=51.00}
 |> checkAccount
-
+ //Users added to record and then passed to checkAccount method
 User {AccountNumber=0105; Balance=100.00}
 |> checkAccount
-
+ //Users added to record and then passed to checkAccount method
 User {AccountNumber=0106; Balance=150.00}
 |> checkAccount
-
-User {AccountNumber=0106; Balance=75.00}
+ //Users added to record and then passed to checkAccount method
+User {AccountNumber=0107; Balance=75.00}
 |> checkAccount
 
-//let user1 = User {Balance = 10.00; AccountNumber = 0104;}
-//|> checkAccount
-
-
-
-
-(*
-type Account =
-    | AccountNumber of int
-    | Balance of float
-    | Name of string
-
-//Account holders information
-let joe = Balance(5.00)
-let mark = Balance(50.00)
-let carl = Balance(150.00)
-let mike = Balance(25.00)
-let james = Balance(200.00)
-let andy = Balance(8.00)
-
-let accounts = [joe; mark; carl; mike; james; andy;]
-
-//Method thats checks balance and displays appropriate message
-let CheckAccount v =
-    for a in accounts do
-    match a with
-    | Balance(float)->if (float < 10.00) then
-                         Console.WriteLine("Balance is Low")
-                      elif (float >= 10.00 && float <= 100.00) then
-                         Console.WriteLine("Balance is Ok")
-                      elif (float > 100.00) then 
-                         Console.WriteLine("Balance is High")
-
-CheckAccount accounts
-*)
